bd07d2afeb53ff8ac45a4178b2fc962e55f931d2
joeylaguna <elagunaj@gmail.com>
2017-08-03 10:51:27 -0700

    clean up server functions to controllers

---
+++ server/controllers/index.js
@@ -0,0 +1,54 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: 'localhost',
+    user: 'admin',
+    password: '123',
+    database: 'Joey',
+    charset: 'utf8'
+  }
+});
+const bookshelf = require('bookshelf')(knex);
+const Users = bookshelf.Model.extend({
+  tableName: 'users'
+});
+
+module.exports = {
+  postFormOne: ((req, res) => {
+    let username = req.params.username;
+    let password = req.params.password;
+    let email = req.params.email;
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      bcrypt.hash(password, salt, (err, hash) => {
+        new Users({username: username, password: hash, email: email}).save().then((model) => {
+          res.send(hash);
+        })
+      })
+    }) 
+  }),
+  postFormTwo: ((req, res) => {
+    let id = req.params.id;
+    let firstName = req.params.firstName;
+    let lastName = req.params.lastName;
+    let phoneNumber = req.params.phoneNumber;
+    Users.forge().where({id: id}).save({firstname: firstName, lastname: lastName, phonenumber: phoneNumber}, {method: 'update'});
+    res.sendStatus(200);
+  }),
+  postFormThree: ((req, res) => {
+    let id = req.params.id;
+    let address = req.params.address;
+    let city = req.params.city;
+    let state = req.params.state;
+    let zip = req.params.zip;
+    Users.forge().where({id: id}).save({address: address, city: city, state: state, zip: zip}, {method: 'update'});
+    res.sendStatus(200);
+  }),
+  getUserID: ((req, res) => {
+    Users.forge().orderBy('id', 'DESC').fetch()
+    .then((rows) => {
+      res.send(rows.id);
+    });
+  })
+}
--- server/server.js
+++ server/server.js
@@ -1,67 +1,14 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const knex = require('knex')({
-  client: 'pg',
-  connection: {
-    host: 'localhost',
-    user: 'admin',
-    password: '123',
-    database: 'Joey',
-    charset: 'utf8'
-  }
-});
-const bookshelf = require('bookshelf')(knex);
-const Users = bookshelf.Model.extend({
-  tableName: 'users'
-});
 const app = express();
+const controller = require('./controllers');
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/../public'));
 
-app.post('/addUsers/:username/:password/:email', (req, res) => {
-  console.log('fadfdad');
-  let username = req.params.username;
-  let password = req.params.password;
-  let email = req.params.email;
-  bcrypt.genSalt(saltRounds, (err, salt) => {
-    bcrypt.hash(password, salt, (err, hash) => {
-      new Users({username: username, password: hash, email: email}).save().then((model) => {
-        res.send(hash);
-      })
-    })
-  })
-  console.log(`${username} ${password}`)
-  //Users.save({username: req.params.username, password: req.params.password});
-  
-});
-
-app.post('/addFormTwo/:id/:firstName/:lastName/:phoneNumber', (req, res) => {
-  let id = req.params.id;
-  let firstName = req.params.firstName;
-  let lastName = req.params.lastName;
-  let phoneNumber = req.params.phoneNumber;
-  Users.forge().where({id: id}).save({firstname: firstName, lastname: lastName, phonenumber: phoneNumber}, {method: 'update'});
-  res.sendStatus(200);
-})
-
-app.post('/addFormThree/:id/:address/:city/:state/:zip', (req, res) => {
-  let id = req.params.id;
-  let address = req.params.address;
-  let city = req.params.city;
-  let state = req.params.state;
-  let zip = req.params.zip;
-  Users.forge().where({id: id}).save({address: address, city: city, state: state, zip: zip}, {method: 'update'});
-  res.sendStatus(200);
-})
-
-app.get('/getUserID', (req, res) => {
-  Users.forge().orderBy('id', 'DESC').fetch()
-    .then((rows) => {
-      res.send(rows.id);
-    });
-});
+app.post('/addUsers/:username/:password/:email', controller.postFormOne );
+app.post('/addFormTwo/:id/:firstName/:lastName/:phoneNumber', controller.postFormTwo);
+app.post('/addFormThree/:id/:address/:city/:state/:zip', controller.postFormThree);
+app.get('/getUserID', controller.getUserID);
 
 app.get('/', (req,res) => {
   console.log('fdsfdsaf');
